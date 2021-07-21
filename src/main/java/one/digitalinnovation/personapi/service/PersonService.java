5eb68939ab03e218623f4526f1b29900b570dfc8
schott-anderson <schott.anderson@hotmail.com>
2021-07-20 23:51:01 -0300

    Improved person creation validation funcionality

---
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -0,0 +1,26 @@
+package one.digitalinnovation.personapi.service;
+
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
+import one.digitalinnovation.personapi.repository.PersonRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
+            return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
